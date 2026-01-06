aaf1054a639bb4fcf5e2c601e8d3a85c503c5ca8
Meghma Das <meghmadas@mcsinfo.in>
2026-01-06 21:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ const connection = mysql.createConnection({
   password: '',
 });
 
+
+//const getRandomUser = () => ([
+//  faker.string.uuid(),
+//  faker.internet.username(),
+//  faker.internet.email(),
+//  faker.internet.password(),
+//]);
+
+
+//const q = "INSERT INTO user (id, username, email, password) VALUES ?";
+//let data = [];
+//for (let i = 0; i < 100; i++) {
+//  data.push(getRandomUser());
+//}
+//connection.query(q, [data], (err, result) => {
+//  if (err) {
+//    console.error("Insert error:", err);
+//  } else {
+//    console.log("Rows inserted:", result.affectedRows);
+//  }
+//  connection.end(); 
+//});
+
+
 /* ---------------- HOME ---------------- */
 app.get("/", (req, res) => {
   let q = "SELECT COUNT(*) AS count FROM user";
@@ -147,107 +171,5 @@ app.listen(8080, () => {
   console.log("Server is listening on port 8080");
 });
 
-//const getRandomUser = () => ([
-//  faker.string.uuid(),
-//  faker.internet.username(),
-//  faker.internet.email(),
-//  faker.internet.password(),
-//]);
 
 
-//const q = "INSERT INTO user (id, username, email, password) VALUES ?";
-//let data = [];
-//for (let i = 0; i < 100; i++) {
-//  data.push(getRandomUser());
-//}
-//connection.query(q, [data], (err, result) => {
-//  if (err) {
-//    console.error("Insert error:", err);
-//  } else {
-//    console.log("Rows inserted:", result.affectedRows);
-//  }
-//  connection.end(); 
-//});
-
-//app.get("/", (req, res) => {
-//  let q = "SELECT COUNT(*) AS count FROM user";
-//
-//  connection.query(q, (err, result) => {
-//    if (err) {
-//      console.log(err);
-//      return res.send("Some error in Database");
-//    }
-//
-//    let count = result[0].count;
-//    res.render("home.ejs", { count });
-//  });
-//});
-//
-//app.get("/user", (req, res) => {
-//  let q = "SELECT * FROM user";
-//  connection.query(q, (err, users) => {
-//    if (err) {
-//      console.log(err);
-//      return res.send("Some error in Database");
-//    }
-//    res.render("show.ejs", { users });
-//  });
-//});
-//
-//app.get("/user/:id/edit", (req, res) => {
-//  let { id } = req.params;
-//
-//  let q = `SELECT * FROM user WHERE id='${id}'`;
-//
-//  connection.query(q, (err, result) => {
-//    if (err) {
-//      console.log(err);
-//      return res.send("Some error in Database");
-//    }
-//
-//    let user = result[0]; 
-//    res.render("edit.ejs", { user });
-//  });
-//});
-//
-//app.patch("/user/:id", (req, res) => {
-//  let { id } = req.params;
-//  let { password: formPass, username: newUsername } = req.body;
-//
-//  let q = "SELECT * FROM user WHERE id = ?";
-//
-//  connection.query(q, [id], (err, result) => {
-//    if (err) {
-//      console.log(err);
-//      return res.send("Some error in Database");
-//    }
-//
-//    if (result.length === 0) {
-//      return res.send("User not found");
-//    }
-//
-//    let user = result[0];
-//    if (formPass !== user.password) {
-//      return res.send("Wrong Password");
-//    }
-//    let q2 = "UPDATE user SET username = ? WHERE id = ?";
-//
-//    connection.query(q2, [newUsername, id], (err) => {
-//      if (err) {
-//        console.log(err);
-//        return res.send("Some error in Database");
-//      }
-//      res.redirect("/user");
-//    });
-//  });
-//});
-//
-//app.get("/user/new", (req, res) => {
-//    res.render("new.ejs");
-//});
-//
-//app.listen(8080, () => {
-//  console.log("Server is listening on port 8080");
-//});
-
-//
